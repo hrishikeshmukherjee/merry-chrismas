4ff8f85255f918ef36278b47dfc466fbfb22a77c
Hrishikesh Mukherjee <110320501+hrishikeshmukherjee@users.noreply.github.com>
2022-12-25 22:27:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'forms/config.php'; ?>
+<head>
+    <title>Merry Christmas </title>
+  <?php include 'link.php'; ?>
+</head>
+<body style="background-image: url('assets/img/1.jpg'); background-size:100%;">
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+      <h1 class="logo"><a href="index.html"><font><b><b>Merry Chrismas -- 2022 SIA</b></b></font></a></h1>
+    </div>
+  </header>
+  <main id="main">
+    <section class="section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-md-10">
+        <div class="card" style="background-image: url('assets/img/gg.jpg'); background-size:100%;">
+          <h1><b><marquee scrollamount="11"><font face="Lucida Calligraphy"><img src="assets/img/5.jpg" alt="">Merry Chrismas 2022</font><img src="assets/img/5.jpg" alt=""></marquee></b></h1>
+          <div class="card-body">
+            <p>
+              <font color="red" face="Lucida Calligraphy"><b><h3>
+              “The distance is just a number when you are connected with your friend with your heart and soul. Warm greetings on Christmas to my long distance friend.”<br><br>
+
+              <center><img src="assets/img/sia.jpg" alt="sia" style="height:500px; width:350px;"><br><br></center>
+
+              “Even the greatest of the distance can not fade away the vibes of Christmas for both of us because we are bonded with love. Merry Christmas to you.”<br><br>
+
+            “Wishing a blessed and beautiful Christmas to you my friend. I really wish we were together to celebrate this occasion with each other.”<br><br></h3></b></font>
+            </p>
+
+            <video width="150" height="250" controls>
+              <source src="assets/img/movie.mp4" type="video/mp4">
+              </video>
+              
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+
+  </main>
+
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
